010423147be67766ea31a5105b8859ad62ca5e1b
Iskaa303 <iska060808@gmail.com>
2025-07-18 17:29:48 -0400

    Added Session Middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div>
-      <Input />
-      <Button>
-        Primary
-      </Button>
-      <Button variant="secondary">
-        Secondary
-      </Button>
-      <Button variant="ghost">
-        Ghost
-      </Button>
-      <Button variant="link">
-        Link
-      </Button>
-      <Button disabled>
-        Disabled
-      </Button>
-      <Button variant="destructive">
-        Destructive
-      </Button>
-      <Button variant="outline">
-        Outline
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
       </Button>
     </div>
   );
-}
+};
